f94a06d85b089ecbaa959398e42b5b71732e4c49
Jesse Arstein <jesse.arstein@yahoo.com>
2018-09-25 09:16:26 -0600

    Commit the project

---
+++ TicketMonitor/loginWindow.Designer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace TicketMonitor
+{
+    partial class loginFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginFrame));
+            this.msuLogo = new System.Windows.Forms.PictureBox();
+            this.userNameField = new System.Windows.Forms.TextBox();
+            this.passwordField = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.msuLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // msuLogo
+            // 
+            this.msuLogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("msuLogo.BackgroundImage")));
+            this.msuLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.msuLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.msuLogo.Location = new System.Drawing.Point(98, 12);
+            this.msuLogo.Name = "msuLogo";
+            this.msuLogo.Size = new System.Drawing.Size(175, 140);
+            this.msuLogo.TabIndex = 0;
+            this.msuLogo.TabStop = false;
+            this.msuLogo.Click += new System.EventHandler(this.msuLogo_Click);
+            // 
+            // userNameField
+            // 
+            this.userNameField.Location = new System.Drawing.Point(75, 230);
+            this.userNameField.Name = "userNameField";
+            this.userNameField.Size = new System.Drawing.Size(225, 20);
+            this.userNameField.TabIndex = 1;
+            // 
+            // passwordField
+            // 
+            this.passwordField.Location = new System.Drawing.Point(75, 278);
+            this.passwordField.Name = "passwordField";
+            this.passwordField.Size = new System.Drawing.Size(225, 20);
+            this.passwordField.TabIndex = 2;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(72, 210);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(73, 17);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "Username";
+            this.usernameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(75, 259);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(69, 17);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password";
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.Gold;
+            this.loginButton.Location = new System.Drawing.Point(98, 357);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(175, 34);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // loginFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(384, 486);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passwordField);
+            this.Controls.Add(this.userNameField);
+            this.Controls.Add(this.msuLogo);
+            this.MaximizeBox = false;
+            this.Name = "loginFrame";
+            this.Text = "Ticket Monitor Login";
+            ((System.ComponentModel.ISupportInitialize)(this.msuLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void msuLogo_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox msuLogo;
+        private System.Windows.Forms.TextBox userNameField;
+        private System.Windows.Forms.TextBox passwordField;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
