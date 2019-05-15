dd76caef587e0fb0b45e1f37e5a8cce7e55f258a
Jesse Arstein <jesse.arstein@yahoo.com>
2019-05-15 16:16:25 -0600

    Added Option Menu and Functional Progress Bar Foundation for ticket gathering is ready.

--- TicketMonitor/TicketMonitor.Designer.cs
+++ TicketMonitor/TicketMonitor.Designer.cs
@@ -28,27 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.commandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.manualRefreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.monitorOutputTextBox = new System.Windows.Forms.RichTextBox();
             this.progressbarLabel = new System.Windows.Forms.Label();
             this.debugGet = new System.Windows.Forms.Button();
+            this.optionPanel = new System.Windows.Forms.Panel();
+            this.optionSaveButton = new System.Windows.Forms.Button();
+            this.refreshTimeLabel = new System.Windows.Forms.Label();
+            this.refreshTime = new System.Windows.Forms.ComboBox();
             this.menuStrip1.SuspendLayout();
+            this.optionPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // progressBar1
+            // progressBar
             // 
-            this.progressBar1.Location = new System.Drawing.Point(418, 415);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(348, 23);
-            this.progressBar1.TabIndex = 0;
+            this.progressBar.Location = new System.Drawing.Point(418, 415);
+            this.progressBar.Maximum = 30000;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(348, 23);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 0;
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.commandsToolStripMenuItem});
+            this.commandsToolStripMenuItem,
+            this.optionsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(800, 24);
@@ -68,6 +77,13 @@
             this.manualRefreshToolStripMenuItem.Name = "manualRefreshToolStripMenuItem";
             this.manualRefreshToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
             this.manualRefreshToolStripMenuItem.Text = "Manual Refresh";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            this.optionsToolStripMenuItem.Click += new System.EventHandler(this.OptionsToolStripMenuItem_Click);
             // 
             // monitorOutputTextBox
             // 
@@ -100,6 +116,55 @@
             this.debugGet.UseVisualStyleBackColor = true;
             this.debugGet.Click += new System.EventHandler(this.DebugGet_Click);
             // 
+            // optionPanel
+            // 
+            this.optionPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.optionPanel.Controls.Add(this.optionSaveButton);
+            this.optionPanel.Controls.Add(this.refreshTimeLabel);
+            this.optionPanel.Controls.Add(this.refreshTime);
+            this.optionPanel.Location = new System.Drawing.Point(0, 0);
+            this.optionPanel.Name = "optionPanel";
+            this.optionPanel.Size = new System.Drawing.Size(800, 452);
+            this.optionPanel.TabIndex = 5;
+            this.optionPanel.Visible = false;
+            // 
+            // optionSaveButton
+            // 
+            this.optionSaveButton.Location = new System.Drawing.Point(281, 365);
+            this.optionSaveButton.Name = "optionSaveButton";
+            this.optionSaveButton.Size = new System.Drawing.Size(221, 31);
+            this.optionSaveButton.TabIndex = 2;
+            this.optionSaveButton.Text = "Save";
+            this.optionSaveButton.UseVisualStyleBackColor = true;
+            this.optionSaveButton.Click += new System.EventHandler(this.OptionSaveButton_Click);
+            // 
+            // refreshTimeLabel
+            // 
+            this.refreshTimeLabel.AutoSize = true;
+            this.refreshTimeLabel.BackColor = System.Drawing.Color.SteelBlue;
+            this.refreshTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refreshTimeLabel.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.refreshTimeLabel.Location = new System.Drawing.Point(30, 24);
+            this.refreshTimeLabel.Name = "refreshTimeLabel";
+            this.refreshTimeLabel.Size = new System.Drawing.Size(89, 16);
+            this.refreshTimeLabel.TabIndex = 1;
+            this.refreshTimeLabel.Text = "Refresh Time";
+            // 
+            // refreshTime
+            // 
+            this.refreshTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.refreshTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.refreshTime.FormattingEnabled = true;
+            this.refreshTime.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.refreshTime.Items.AddRange(new object[] {
+            "One Minute",
+            "Five Minutes",
+            "Ten Minutes"});
+            this.refreshTime.Location = new System.Drawing.Point(136, 21);
+            this.refreshTime.Name = "refreshTime";
+            this.refreshTime.Size = new System.Drawing.Size(115, 23);
+            this.refreshTime.TabIndex = 0;
+            // 
             // ticketMonitorFrame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -109,28 +174,39 @@
             this.Controls.Add(this.debugGet);
             this.Controls.Add(this.progressbarLabel);
             this.Controls.Add(this.monitorOutputTextBox);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.progressBar);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.optionPanel);
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "ticketMonitorFrame";
             this.Text = "Ticket Monitor";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ticketMonitorFrame_FormClosed);
             this.Load += new System.EventHandler(this.TicketMonitorFrame_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.optionPanel.ResumeLayout(false);
+            this.optionPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+        //Anything above here will be generated over.
 
-        private System.Windows.Forms.ProgressBar progressBar1;
+
+        private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem commandsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem manualRefreshToolStripMenuItem;
         private System.Windows.Forms.RichTextBox monitorOutputTextBox;
         private System.Windows.Forms.Label progressbarLabel;
         private System.Windows.Forms.Button debugGet;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.Panel optionPanel;
+        private System.Windows.Forms.ComboBox refreshTime;
+        private System.Windows.Forms.Button optionSaveButton;
+        private System.Windows.Forms.Label refreshTimeLabel;
     }
 }
