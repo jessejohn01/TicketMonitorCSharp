a222045ad080436317511ab6da373d5b61c0c346
Jesse Arstein <jesse.arstein@yahoo.com>
2019-06-19 13:13:49 -0600

    Login process changed

--- TicketMonitor/programPackage.Designer.cs
+++ TicketMonitor/programPackage.Designer.cs
@@ -33,10 +33,6 @@ namespace TicketMonitor
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(programPackage));
             this.msuLogo = new System.Windows.Forms.PictureBox();
-            this.userNameField = new System.Windows.Forms.TextBox();
-            this.apiKeyField = new System.Windows.Forms.TextBox();
-            this.usernameLabel = new System.Windows.Forms.Label();
-            this.passwordLabel = new System.Windows.Forms.Label();
             this.loginButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.msuLogo)).BeginInit();
             this.SuspendLayout();
@@ -53,47 +49,12 @@ namespace TicketMonitor
             this.msuLogo.TabStop = false;
             this.msuLogo.Click += new System.EventHandler(this.msuLogo_Click);
             // 
-            // userNameField
-            // 
-            this.userNameField.Location = new System.Drawing.Point(72, 245);
-            this.userNameField.Name = "userNameField";
-            this.userNameField.Size = new System.Drawing.Size(225, 20);
-            this.userNameField.TabIndex = 1;
-            // 
-            // apiKeyField
-            // 
-            this.apiKeyField.Location = new System.Drawing.Point(72, 293);
-            this.apiKeyField.Name = "apiKeyField";
-            this.apiKeyField.PasswordChar = '*';
-            this.apiKeyField.Size = new System.Drawing.Size(225, 20);
-            this.apiKeyField.TabIndex = 2;
-            // 
-            // usernameLabel
-            // 
-            this.usernameLabel.AutoSize = true;
-            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameLabel.Location = new System.Drawing.Point(69, 225);
-            this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(73, 17);
-            this.usernameLabel.TabIndex = 3;
-            this.usernameLabel.Text = "Username";
-            // 
-            // passwordLabel
-            // 
-            this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordLabel.Location = new System.Drawing.Point(72, 274);
-            this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(57, 17);
-            this.passwordLabel.TabIndex = 4;
-            this.passwordLabel.Text = "API Key";
-            // 
             // loginButton
             // 
             this.loginButton.BackColor = System.Drawing.Color.Gold;
-            this.loginButton.Location = new System.Drawing.Point(95, 372);
+            this.loginButton.Location = new System.Drawing.Point(95, 271);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(175, 34);
+            this.loginButton.Size = new System.Drawing.Size(175, 44);
             this.loginButton.TabIndex = 5;
             this.loginButton.Text = "Login";
             this.loginButton.UseVisualStyleBackColor = false;
@@ -106,17 +67,12 @@ namespace TicketMonitor
             this.BackColor = System.Drawing.Color.SteelBlue;
             this.ClientSize = new System.Drawing.Size(376, 486);
             this.Controls.Add(this.loginButton);
-            this.Controls.Add(this.passwordLabel);
-            this.Controls.Add(this.usernameLabel);
-            this.Controls.Add(this.apiKeyField);
-            this.Controls.Add(this.userNameField);
             this.Controls.Add(this.msuLogo);
             this.MaximizeBox = false;
             this.Name = "programPackage";
             this.Text = "Ticket Monitor Login";
             ((System.ComponentModel.ISupportInitialize)(this.msuLogo)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -133,10 +89,6 @@ namespace TicketMonitor
         #endregion
 
         private System.Windows.Forms.PictureBox msuLogo;
-        private System.Windows.Forms.TextBox userNameField;
-        private System.Windows.Forms.TextBox apiKeyField;
-        private System.Windows.Forms.Label usernameLabel;
-        private System.Windows.Forms.Label passwordLabel;
         private System.Windows.Forms.Button loginButton;
     }
 }
--- TicketMonitor/programPackage.cs
+++ TicketMonitor/programPackage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,21 +18,100 @@ namespace TicketMonitor
         internal programPackage()
         {
             InitializeComponent();
-            this.apiKeyField.Paste(Clipboard.GetText());
 
         }
 
         private void loginButton_Click(object sender, EventArgs e) //If the login button is clicked then do this.
-        { 
+        {
+            checkUserFirstLogin();
             //Will store credentials in the user class.
-            user.setUsername(userNameField.Text);
-            user.setapiKey(apiKeyField.Text);
+            //user.setUsername(userNameField.Text);
+            //user.setapiKey(apiKeyField.Text);
             monitor.Show();
             this.Hide();
             monitor.updateText("Using " + user.getUsername() + " as the username.");
 
         }
 
+        private void checkUserFirstLogin()
+        {
+            List<string> userList = getUserList();
+            List<string> apiKeyList = getApiKeyList();
+            if (userList.Contains(Environment.UserName))
+            {
+                Console.WriteLine("Returning user");
+                user.setUsername(Environment.UserName);
+
+                for(int i = 0; i<userList.Count; i++)
+                {
+                    if(userList[i] == Environment.UserName)
+                    {
+                        user.setapiKey(apiKeyList[i]);
+                    }
+                }
+
+            }
+            else
+            {
+                //New user process goes here.
+                newUser newUserWindow = new newUser(); //Creates the window.
+                newUserWindow.ShowDialog();
+
+
+            }
+        }
+
+        private List<String> getUserList()
+        {
+            
+            List<String> userList = new List<String>();
+            try
+            {
+                var lines = File.ReadLines("users.txt");
+                foreach (var line in lines)
+                {
+                    userList.Add(line); //Adds each user.
+                }
+
+
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("First time user.");
+            }
+
+            return userList;
+        }
+
+        private List<String> getApiKeyList()
+        {
+
+            List<String> apiKeyList = new List<String>();
+            try
+            {
+                var lines = File.ReadLines("apiKeys.txt");
+                foreach (var line in lines)
+                {
+                    apiKeyList.Add(line); //Adds each user.
+                }
+                using (System.IO.StreamReader file = new System.IO.StreamReader("apiKeys.txt"))
+                {
+                    while (!file.EndOfStream)
+                    {
+                        apiKeyList.Add(file.ReadLine());
+                    }
+                }
+
+
+            }
+            catch (Exception)
+            {
+               // Console.WriteLine("File Not Found. Something went wrong.");
+               // MessageBox.Show("A critical error has occured. ");
+            }
+
+            return apiKeyList;
+        }
 
     }
 }
